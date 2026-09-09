5981973a6739b971a0aa1cf8155f445627c105ad
sinagossler00 <sinagossler00@gmail.com>
2026-09-10 01:02:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,28 @@
-const CACHE = 'my-studyplaner-pwa-v1';
+const CACHE = 'my-studyplaner-pwa-v252';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './icons/icon-192-maskable.png',
+  './icons/icon-512-maskable.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   const req = event.request;
   const url = new URL(req.url);
-
-  // Only handle GET requests from the same app origin.
   if (req.method !== 'GET' || url.origin !== self.location.origin) return;
-
   event.respondWith(
     fetch(req).then(response => {
       const copy = response.clone();
