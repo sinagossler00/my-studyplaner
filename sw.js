fb4b5b86a5aba6f652290236d3a5e8d260e533dd
sinagossler00 <sinagossler00@gmail.com>
2026-09-10 00:44:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'my-studyplaner-pwa-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  // Only handle GET requests from the same app origin.
+  if (req.method !== 'GET' || url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(req).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(req, copy));
+      return response;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
