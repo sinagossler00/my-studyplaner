661d4730f9bf0dcac5437170004d9f7264940a4b
sinagossler00 <sinagossler00@gmail.com>
2026-09-10 14:50:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-studyplaner-pwa-v263-font-files-clean-clean';
+const CACHE = 'my-studyplaner-pwa-v264-font-final-files-clean-clean';
 const APP_SHELL = [
   './',
   './index.html',
